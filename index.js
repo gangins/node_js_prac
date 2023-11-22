eb170e7af0f1af25ee58074070aa640c7d7ed712
gangins <kki129443@gmail.com>
2023-11-22 11:38:14 +0900

    feat: 연습

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express') //익스프레스 모듈 갖고오고
 const app = express() //새로운 앱만들고
-const port = 3000 //포트
+const port = 5000 //포트
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+const {User} = require('./models/User')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test:12341234@boilerplate.c2oszcx.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI,{
+  useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false
+})
 .then(()=>{
     console.log('연결완료')
 })
@@ -14,6 +23,17 @@ mongoose.connect('mongodb+srv://test:12341234@boilerplate.c2oszcx.mongodb.net/?r
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.post('/register', async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    const doc = await user.save();
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.json({ success: false, err });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
